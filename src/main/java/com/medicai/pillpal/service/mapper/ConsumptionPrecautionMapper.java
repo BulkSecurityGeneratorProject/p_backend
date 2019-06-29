aaea7e9d228a1d439a381bff53d75749849d45f6
mjv703 <mjv703@yahoo.com>
2019-06-29 16:43:53 +0430

    database change logs

---
+++ src/main/java/com/medicai/pillpal/service/mapper/ConsumptionPrecautionMapper.java
@@ -0,0 +1,28 @@
+package com.medicai.pillpal.service.mapper;
+
+import com.medicai.pillpal.domain.*;
+import com.medicai.pillpal.service.dto.ConsumptionPrecautionDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity {@link ConsumptionPrecaution} and its DTO {@link ConsumptionPrecautionDTO}.
+ */
+@Mapper(componentModel = "spring", uses = {ApplicationInfoMapper.class})
+public interface ConsumptionPrecautionMapper extends EntityMapper<ConsumptionPrecautionDTO, ConsumptionPrecaution> {
+
+    @Mapping(source = "applicationInfo.id", target = "applicationInfoId")
+    ConsumptionPrecautionDTO toDto(ConsumptionPrecaution consumptionPrecaution);
+
+    @Mapping(source = "applicationInfoId", target = "applicationInfo")
+    ConsumptionPrecaution toEntity(ConsumptionPrecautionDTO consumptionPrecautionDTO);
+
+    default ConsumptionPrecaution fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        ConsumptionPrecaution consumptionPrecaution = new ConsumptionPrecaution();
+        consumptionPrecaution.setId(id);
+        return consumptionPrecaution;
+    }
+}
